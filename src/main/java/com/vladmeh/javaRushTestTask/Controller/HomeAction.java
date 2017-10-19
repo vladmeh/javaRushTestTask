a9ec5dfab0c1c38431754aa40ba9b4562e6c35a7
VladMeh <vladmeh@gmail.com>
2017-10-19 16:33:12 +0300

    Первый конторолеер и первое тестирование

---
+++ src/main/java/com/vladmeh/javaRushTestTask/Controller/HomeAction.java
@@ -0,0 +1,13 @@
+package com.vladmeh.javaRushTestTask.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeAction {
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello World";
+    }
+}
