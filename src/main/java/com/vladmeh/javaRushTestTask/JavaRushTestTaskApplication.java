de7068c267681004c04419305dcc000c858934c9
VladMeh <vladmeh@gmail.com>
2017-10-19 12:33:53 +0300

    Уставновка Spring

---
+++ src/main/java/com/vladmeh/javaRushTestTask/JavaRushTestTaskApplication.java
@@ -0,0 +1,12 @@
+package com.vladmeh.javaRushTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRushTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRushTestTaskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vladmeh/javaRushTestTask/JavaRushTestTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vladmeh.javaRushTestTask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaRushTestTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
